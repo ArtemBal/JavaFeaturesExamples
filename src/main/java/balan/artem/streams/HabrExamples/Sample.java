01fd36bd7b9f3fd31146eefbf6bcfc35ecf2a4d0
Artem Balan <a.s.balan96@gmail.com>
2023-01-23 23:21:58 +0200

    Added 6 more tests

--- src/main/java/balan/artem/streams/HabrExamples/Sample.java
+++ src/main/java/balan/artem/streams/HabrExamples/Sample.java
@@ -1,8 +1,6 @@
 package balan.artem.streams.HabrExamples;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Sample {
@@ -23,6 +21,12 @@ public class Sample {
         sample.test2();
         sample.test3();
         sample.test4();
+        sample.test5();
+        sample.test6();
+        sample.test7();
+        sample.test8();
+        sample.test9();
+        sample.test10();
     }
 
     /**
@@ -77,5 +81,84 @@ public class Sample {
                         .thenComparing(User::getLastName))
                 .collect(Collectors.toList())
                 .forEach(System.out::println);
+    }
+
+    /**
+     * get average age and max first name length
+     */
+    private void test5() {
+        System.out.println("Test 5");
+        double averageAge = userList.stream()
+                .mapToInt(User::getAge)
+                .summaryStatistics()
+                .getAverage();
+        System.out.println("Average age: " + averageAge);
+        int firstNameLength = userList.stream()
+                .mapToInt((value) -> {
+                    return value.getFirstName().length();
+                })
+                .summaryStatistics()
+                .getMax();
+        System.out.println("max first name length: " + firstNameLength);
+    }
+
+    /**
+     * check if all ages less than 79
+     */
+    private void test6() {
+        System.out.println("Test 6");
+        boolean isAllAgesLess79 = userList.stream()
+                .allMatch((user -> user.getAge() < 79));
+        System.out.println("Is all users age less than 79: " + isAllAgesLess79);
+    }
+
+    /**
+     * check if any first name starts with M char
+     */
+    private void test7() {
+        System.out.println("Test 8");
+        boolean isFirstCharS = userList.stream()
+                .anyMatch((user -> user.getFirstName().charAt(0) == 'M'));
+        System.out.println("Is any first name starts with M: " + isFirstCharS);
+    }
+
+    /**
+     * transform into another collection
+     */
+    private void test8() {
+        System.out.println("Test 9");
+        List<User> list = userList.stream()
+                .collect(Collectors.toList());
+        Set<User> set = userList.stream()
+                .collect(Collectors.toSet());
+        List<User> linkedList = userList.stream()
+                .collect(Collectors.toCollection(LinkedList::new));
+        Map<Long, User> map = userList.stream()
+                .collect(Collectors.toMap(user -> user.getId(), user -> user));
+    }
+
+    /**
+     * count different nationalities
+     */
+    private void test9() {
+        System.out.println("Test 9");
+        long countDifNationalities = userList.stream()
+                .map(User::getNationality)
+                .distinct()
+                .count();
+        System.out.println("count of different nationalities: " + countDifNationalities);
+    }
+
+
+    /**
+     * filter users by few parameters
+     */
+    private void test10() {
+        System.out.println("Test 10");
+        userList.stream()
+                .filter(p -> (p.getFirstName().charAt(0) != 'M'))
+                .filter(p -> (p.getAge() > 10))
+                .forEach(System.out::println);
+
     }
 }
