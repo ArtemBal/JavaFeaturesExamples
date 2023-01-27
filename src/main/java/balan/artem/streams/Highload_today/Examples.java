52526ed9cd30d19dbd1c9c569b32881b8f5d031e
Artem Balan <a.s.balan96@gmail.com>
2023-01-27 23:44:03 +0200

    More complex exercises with stream API (Highload.today)

--- src/main/java/balan/artem/streams/Highload_today/Examples.java
+++ src/main/java/balan/artem/streams/Highload_today/Examples.java
@@ -2,18 +2,22 @@ package balan.artem.streams.Highload_today;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Examples {
 
-    List<String> myList = Arrays.asList("Highload", "High", "Load", "Highload");
+
 
     public static void main(String[] args) {
         Examples examples = new Examples();
         examples.test1();
+        examples.test2();
     }
 
     private void test1() {
         System.out.println("Test 1");
+        List<String> myList = Arrays.asList("Highload", "High", "Load", "Highload");
+
         myList.stream().map(s -> s + " ").forEach(System.out::print);
         System.out.println("\nHigh occurs " + myList.stream().filter("High"::equals).count() + " times");
         System.out.println("First element of collection: " + myList.stream().findFirst().orElse("\"Empty collection\""));
@@ -24,6 +28,25 @@ public class Examples {
                 myList.stream().skip(1).findFirst().orElse("\"Empty collection\""));
         System.out.println("Use Skip and Limit to get middle part of collection: " +
                 Arrays.toString(myList.stream().skip(1).limit(2).toArray()));
+    }
+
+    private void test2() {
+        System.out.println("Test 2");
+        List<String> myList = Arrays.asList("f10", "f15", "f2", "f4");
+        System.out.println("Minimal element: " +
+                myList.stream().min(String::compareTo).get());
+        System.out.println("Max element: " +
+                myList.stream().max(String::compareTo).get());
+
+        List<String> duplicateList = Arrays.asList("f10", "f15", "f2", "f4", "f4");
+        System.out.println("Sort and to array methods: " +
+                duplicateList.stream()
+                        .sorted()
+                        .collect(Collectors.toList()));
+        System.out.println("Sort reverse and delete duplicates: " +
+                duplicateList.stream()
+                        .sorted((o1, o2) -> - o1.compareTo(o2)).distinct()
+                        .collect(Collectors.toList()));
     }
 
 }
