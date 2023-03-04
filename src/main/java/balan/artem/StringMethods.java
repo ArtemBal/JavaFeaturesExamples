c1a021e1ff8be3f608460fd56c2d7673b946b56c
Artem Balan <a.s.balan96@gmail.com>
2023-03-04 23:23:55 +0200

    .strip and .isBlank methods with String

--- src/main/java/balan/artem/StringMethods.java
+++ src/main/java/balan/artem/StringMethods.java
@@ -24,8 +24,38 @@ public class StringMethods {
         strings.forEach(out::println);
     }
 
+    public static void methodsStrip() {
+        String originalString1 = " prepared string surrounded by spaces ";
+        methodRepeat("String.strip() on '" + originalString1 + "'");
+        out.println("'" + originalString1.strip() + "'");
+
+        String originalString2 = " prepared string surrounded by spaces ";
+        methodRepeat("String.stripLeading() on '" + originalString2 + "'");
+        out.println("'" + originalString2.stripLeading() + "'");
+
+        String originalString = " prepared string surrounded by spaces ";
+        methodRepeat("String.stripTrailing() on '" + originalString + "'");
+        out.println("'" + originalString.stripTrailing() + "'");
+    }
+
+    public static void methodIsBlank() {
+        methodRepeat("String.isBlank()");
+        String emptyString = "";
+        out.println("Empty String -> " + emptyString.isBlank());
+        String onlyLineSeparator = System.getProperty("line.separator");
+        out.println("Line Separator Only -> " + onlyLineSeparator.isBlank());
+        String tabOnly = "\t";
+        out.println("Tab Only -> " + tabOnly.isBlank());
+        String spacesOnly = "   ";
+        out.println("Spaces Only -> " + spacesOnly.isBlank());
+        String spacesAndSymbols = " S R ";
+        out.println("Spaces and symbols -> " + spacesAndSymbols.isBlank());
+    }
+
     public static void main(String[] args) {
         methodRepeat("AB");
         methodLines();
+        methodsStrip();
+        methodIsBlank();
     }
 }
